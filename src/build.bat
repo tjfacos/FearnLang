18fb5a0c99d3407b811407024d47f652088dce92
Thomas Facos <tjfacos@gmail.com>
2023-12-18 18:50:08 +0000

    Build works, but not from the .bat file

--- src/build.bat
+++ src/build.bat
@@ -1,3 +1,2 @@
-javac -d build main.java
-cd build
-java FearnC
+javac -cp 'parser/build;C:\libraries\javalibs\antlr-4.13.1-complete.jar' -d build main.java
+java -cp 'build;parser/build;C:\libraries\javalibs\antlr-4.13.1-complete.jar' FearnC
