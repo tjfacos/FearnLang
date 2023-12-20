7dcf4a18e70b8147c9423fdd6e9d5b5418649fc7
tjfacos <57414346+tjfacos@users.noreply.github.com>
2023-12-20 17:38:26 +0000

    Tweaked GenANTLR

--- src/cmd/GenANTLR.bat
+++ src/cmd/GenANTLR.bat
@@ -2,8 +2,8 @@
 cd parser
 
 @REM Clear existing generated files
-rm ./gen -r -force
-rm ./build -r -force
+@RD /S /Q "./gen"
+@RD /S /Q "./build"
 
 @REM Generate ANTLR4 Files 
 java -cp C:\\libraries\\javalibs\\antlr-4.13.1-complete.jar org.antlr.v4.Tool -o gen -no-listener -visitor FearnGrammar.g4
