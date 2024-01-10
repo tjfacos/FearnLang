1cc823f0a64f2b06336c06b60e6ff7a542bb627c
tjfacos <57414346+tjfacos@users.noreply.github.com>
2024-01-10 09:12:55 +0000

    Tweaks

--- src/cmd/BuildFearnC.bat
+++
@@ -1,2 +0,0 @@
-javac -cp 'parser/build;C:\libraries\javalibs\antlr-4.13.1-complete.jar' -d build main.java
-@REM java -cp 'build;parser/build;C:\libraries\javalibs\antlr-4.13.1-complete.jar' FearnC
