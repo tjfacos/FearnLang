0c70e5f0476356fc1259227ef38e4c9ae4939513
Thomas Facos <tjfacos@gmail.com>
2024-01-26 19:22:50 +0000

    Added FearnUpdate.cmd

--- release/FearnUpdate.cmd
+++ release/FearnUpdate.cmd
@@ -1,4 +1,17 @@
 @echo off
+
 set jarPath=%~dp0\FearnC.jar
 
-wget  "%jarPath%" %1
+echo Deleting existing FearnC.jar ... 
+
+del "%jarPath%"
+
+echo Complete
+
+echo Downloading latest FearnC.jar ... 
+
+powershell -command "Invoke-WebRequest -Uri https://github.com/tjfacos/FearnLang/raw/main/release/FearnC.jar -Outfile '%jarPath%' -UseBasicParsing"
+
+echo Complete
+
+echo Successfully updated FearnLang Compiler!
